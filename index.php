9246b0e0a0ca3beece3f60fb5f25c39b29e498c7
muriloandrade1 <murilo.andrade1@gmail.com>
2019-01-28 17:01:34 -0200

    correcoes simples

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                                     <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
 						<div class="wrap-btn-slide1 animated visible-false" data-appear="zoomIn">
 							<!-- Button1 -->
-							<a href="#" <img src="images/3.jpeg" alt="IMG-GALLERY">
+							<a href="#" <img src="images/3.jpg" alt="">>
 
 							</a>
 						</div>
