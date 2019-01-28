09215c57f4017f5c9bb686860a86e60cfca0345e
muriloandrade1 <murilo.andrade1@gmail.com>
2019-01-28 17:35:11 -0200

    correcoes simples

--- about.php
+++ about.php
@@ -2,7 +2,7 @@
 	<!-- Title Page -->
 	<section class="bg-title-page flex-c-m p-t-160 p-b-80 p-l-15 p-r-15" style="background-image: url(images/bg-title-page-03.jpg);">
 		<h2 class="tit6 t-center">
-			<img src="images/icons/logo2.png" alt="">
+			<img src="images/icons/logo.png" alt="">
 		</h2>
 	</section>
 
