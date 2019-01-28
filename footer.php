7028175c10bbc896bac3d55809c4faaf8bd11aac
muriloandrade1 <murilo.andrade1@gmail.com>
2019-01-28 21:16:50 -0200

    correcoes simples

--- footer.php
+++ footer.php
@@ -37,10 +37,6 @@
 							e-mail: jwilson@3sw.com.br
 						</li>
 						<li class="txt14 m-b-14">
-							<i class="fa fa-email fs-16 dis-inline-block size19" aria-hidden="true"></i>
-							e-mail: jwilson@3sw.com.br
-						</li>
-						<li class="txt14 m-b-14">
 							<i class="fa fa-whatsapp fs-16 dis-inline-block size19" aria-hidden="true"></i>
 							61- 9 99238 9546 <br>
 							e-mail:everson.costa@3sw.com.br
